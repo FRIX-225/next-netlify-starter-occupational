222f5ddc5b5c1ec424bae093c899b52f6e9854f5
pwussboi <alexkostka@protonmail.com>
2026-03-31 18:50:30 +0200

    reddish dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import '@styles/globals.css'
+import { useEffect } from 'react'
 
 function Application({ Component, pageProps }) {
+  useEffect(() => {
+    const updateTheme = () => {
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+      document.documentElement.classList.toggle('dark', isDark)
+    }
+    
+    updateTheme()
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+    
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', updateTheme)
+    }
+  }, [])
+  
   return <Component {...pageProps} />
 }
 
